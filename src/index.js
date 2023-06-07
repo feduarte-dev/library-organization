25802d7a141b5a89e0c7df2c3b463b9c2e77f824
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-07 18:55:42 -0300

    segundo requisito completo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const { books } = require('../data/library');
 
 // Requisito 1
-const fantasyOrScienceFiction = () => books
-  .filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
+const fantasyOrScienceFiction = () =>
+  books.filter(
+    (book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica',
+  );
 
-const oldBooksOrdered = (year) => {
-  // escreva seu código aqui
-};
+const oldBooksOrdered = (year) =>
+  books
+    .filter((book) => year - book.releaseYear > 60)
+    .sort((bookA, bookB) => bookA.releaseYear - bookB.releaseYear);
+
+// const filterOldBooks = (year) => {
+//   return books.filter((book) => {
+//     const oldBooks = year - book.releaseYear > 60;
+//     return oldBooks;
+//   });
+// };
+
+// const oldBooksOrdered = (year) => {
+//   return filterOldBooks(year).sort((a, b) => {
+//     return a.releaseYear - b.releaseYear;
+//   });
+// };
+
+console.log(oldBooksOrdered(2020));
 
 const booksByAuthorBirthYear = (birthYear) => {
   // escreva seu código aqui
